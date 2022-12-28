53ff88bc9fc427cba5e9ee909de88d95ba3d80a4
Chao Lin <forestschao@gmail.com>
2022-12-27 23:16:20 -0800

    Add config for i3, git, and zsh.

---
+++ by-user/chao/default.nix
@@ -0,0 +1,71 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ./git.nix
+    ./xsession.nix
+    ./ssh.nix
+  ];
+
+  home.packages = with pkgs; [
+    (pkgs.callPackage ../../pkgs/resurrect {})
+  ];
+
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    nix-direnv = {
+      enable = true;
+    };
+  };
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      dracula-theme.theme-dracula
+      yzhang.markdown-all-in-one
+    ];
+  };
+
+  programs.zsh = {
+    enable = true;
+    
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        "pass"     # pass store auto-completion
+        "dotenv"
+        "extract"  # decompression general command
+        "z"
+      ];
+    };
+
+    plugins = [
+      {
+        # will source zsh-autosuggestions.plugin.zsh
+        name = "zsh-autosuggestions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-autosuggestions";
+          rev = "v0.7.0";
+          sha256 = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
+        };
+      }
+      
+      {
+        # will source nix-zsh-completions.plugin.zsh
+        name = "nix-zsh-completions";
+        src = pkgs.fetchFromGitHub {
+          owner = "spwhitt";
+          repo = "nix-zsh-completions";
+          rev = "0.4.4";
+          sha256 = "sha256-Djs1oOnzeVAUMrZObNLZ8/5zD7DjW3YK42SWpD2FPNk=";
+        };
+      }
+    ];
+
+    enableCompletion = true;
+    enableSyntaxHighlighting = true;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A bunch of home-manager modules";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+
+    home-manager.url = "github:nix-community/home-manager/release-22.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: let
+    mkHomeManagerModule = { user, imports }: { config, ... }: {
+      imports = [
+        (home-manager.nixosModules.home-manager)
+      ];
+      home-manager = {
+        # This is needed to make sure that home-manager follows the
+        # pkgs/nixpkgs specified in this flake.
+        #
+        # Relevant github issue: https://github.com/divnix/devos/issues/30
+        useGlobalPkgs = true;
+        useUserPackages = true;
+      };
+      home-manager.users."${user}" = {
+        inherit imports;
+      };
+
+    }; in {
+      nixosModules.chao-home = mkHomeManagerModule {
+        user = "chao";
+        imports = [ ./by-user/chao ];
+      };
+    };
+}
